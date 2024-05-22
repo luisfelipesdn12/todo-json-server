37623d06c2b9f5faf300bf73852f9a30801731d5
Luis Felipe <luisfelipesdn12@gmail.com>
2024-05-21 22:18:30 -0300

    feat: Editar

--- src/app/task.js
+++ src/app/task.js
@@ -11,10 +11,42 @@ export async function deleteTask(taskId) {
     }
 }
 
+export async function updateTask(taskId, task) {
+    try {
+        // 1 - Criandro body em string
+        const body = JSON.stringify(task);
+
+        // 2 - Preparando cabecalho POST
+        const options = {
+            method: 'PUT',
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json"
+            },
+            body: body,
+        }
+
+        // 3 - Fetch
+        let response = await fetch("http://localhost:4002/tasks/" + taskId, options);
+
+        // 4 - Convertendo para JSON
+        let data = await response.json();
+        return data;
+    } catch (error) {
+        console.error("ERROR: updating task");
+    }
+}
+
 export default function Task(props) {
     return (
         <li>
-            <input type="checkbox" checked={props.done} />
+            <input type="checkbox" checked={props.done} onChange={async (e) => {
+                await updateTask(props.id, {
+                    name: props.name,
+                    done: e.target.checked,
+                });
+                await props.readTasks();
+            }} />
             <p>{props.name}</p>
             <button className="delete" onClick={async () => {
                 await deleteTask(props.id);
