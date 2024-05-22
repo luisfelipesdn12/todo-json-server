cbea5426ac8a1bf6ec829e2b2ada915aaec09ffc
Luis Felipe <luisfelipesdn12@gmail.com>
2024-05-21 21:32:45 -0300

    feat: Implemenetação de feito na task

--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,38 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 import Task from "./task";
 
 export default function Home() {
+  const tasks = [
+    {
+      "id": "1",
+      "name": "Tarefa 1",
+      "done": false
+    },
+    {
+      "id": "1",
+      "name": "Tarefa 2",
+      "done": true
+    },
+    {
+      "id": "1",
+      "name": "Tarefa 6",
+      "done": false
+    },
+  ];
+
   return (
     <main className={styles.main}>
       <h1>Lista de tarefas</h1>
       <form>
         <input type="text" placeholder="Digite a tarefa..." />
         <button>
-          <img src="https://super.so/icon/light/plus.svg" alt="+"/>
+          <img src="https://super.so/icon/light/plus.svg" alt="+" />
         </button>
       </form>
       <ul>
-        <Task name="Tarefa 1" />
-        <Task name="Tarefa 2" />
-        <Task name="Tarefa 3" />
-        <Task name="Tarefa 1" />
-        <Task name="Tarefa 1" />
+        {tasks.map(task => (
+          <Task key={task.id} name={task.name} done={task.done} />
+        ))}
       </ul>
     </main>
   );
--- src/app/task.js
+++ src/app/task.js
@@ -1,7 +1,7 @@
 export default function Task(props) {
     return (
         <li>
-            <input type="checkbox" />
+            <input type="checkbox" checked={props.done} />
             <p>{props.name}</p>
             <button className="delete">
                 <img src="https://super.so/icon/light/trash-2.svg" alt="Apagar" />
